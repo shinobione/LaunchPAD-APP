dbcb6f75cda0744d63da3413b0ea1e9d3b79abd8
shinobione <shinobione@users.noreply.github.com>
2026-08-02 18:02:32 +0200

    Bump build and PWA cache for track details

--- js/build-config.js
+++ js/build-config.js
@@ -1,7 +1,7 @@
 (() => {
   const config = Object.freeze({
-    id: '20260802-wave8',
-    cache: 'shinobi-launchpad-v5'
+    id: '20260802-wave9',
+    cache: 'shinobi-launchpad-v6'
   });
 
   globalThis.SHINOBIWAN_BUILD = config;
