2a681d9731e97e2999727dd858e9e8e3c5a57276
ShinoBiWan <shinobione@users.noreply.github.com>
2026-08-06 02:45:44 +0200

    test(layout): retain stable visual-test stylesheet key

--- js/build-config.js
+++ js/build-config.js
@@ -64,7 +64,7 @@
   if (typeof document === 'undefined') return;
 
   const visualTest = new URLSearchParams(location.search).has('visual-test');
-  const stylesheetVersion = visualTest ? '20260806-audiolab-animation-calibration' : config.id;
+  const stylesheetVersion = visualTest ? '20260805-audiolab-core-modes' : config.id;
   if (visualTest) document.documentElement.dataset.visualTest = 'true';
 
   function installAppIconLinks() {
