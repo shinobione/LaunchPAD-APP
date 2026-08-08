279a4e526d6b460f1d5411d024f36fcb3cffc695
ShinoBiWan <shinobione@users.noreply.github.com>
2026-08-08 03:36:17 +0200

    Advance LaunchPAD to Build 54 Gravity Lens

--- js/build-config.js
+++ js/build-config.js
@@ -1,10 +1,10 @@
 (() => {
   const config = Object.freeze({
-    id: '20260808-bass-fracture-v53',
-    cache: 'shinobi-launchpad-v53',
-    revision: 'bass-fracture-1',
-    display: '2026.08.08.53',
-    release: 'bass-fracture-20260808'
+    id: '20260808-gravity-lens-v54',
+    cache: 'shinobi-launchpad-v54',
+    revision: 'gravity-lens-1',
+    display: '2026.08.08.54',
+    release: 'gravity-lens-20260808'
   });
 
   globalThis.SHINOBIWAN_BUILD = config;
